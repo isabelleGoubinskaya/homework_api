0ef2b2df4b05051be74a0e17dc9f7096883919b2
Isabelle <isagoubinskaya@gmail.com>
2023-05-11 23:08:02 +0200

    exercises update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create a stream of integers
+        Stream<Integer> intStream = Stream.of(3, 5, 1, 6, 2);
+
+        // Call findMinMax method with the stream and a comparator
+        MinMaxFinder.findMinMax(
+                intStream,
+                Comparator.naturalOrder(),
+                (min, max) -> {
+                    System.out.println("Minimum element: " + min);
+                    System.out.println("Maximum element: " + max);
+                }
+        );
     }
 }
